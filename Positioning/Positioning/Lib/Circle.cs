fd707c3b433ce6fd1a4ef2d6e47800e221fab1cb
HouHC <1822432547@qq.com>
2019-02-27 19:50:00 +0800

    完善计算两圆交点的方法

--- Positioning/Positioning/Lib/Circle.cs
+++ Positioning/Positioning/Lib/Circle.cs
@@ -31,15 +31,25 @@ namespace Positioning.Lib
                 //交点之间连线的斜率
                 double k2 = -(1.0 / k1);
                 //圆心之间连线与焦点之间连线的交点坐标
-                double x = first.Center.XPosition + ((Math.Pow(2, first.Radius) - Math.Pow(2, second.Radius) + Math.Pow(2, t.Item2)) / (2 * Math.Pow(2, t.Item2))) * (second.Center.XPosition - first.Center.XPosition);
-                double y = first.Center.YPOsition + ((Math.Pow(2, first.Radius) - Math.Pow(2, second.Radius) + Math.Pow(2, t.Item2)) / (2 * Math.Pow(2, t.Item2))) * (second.Center.YPOsition - first.Center.YPOsition);
+                double xe = first.Center.XPosition + ((Math.Pow(2, first.Radius) - Math.Pow(2, second.Radius) + Math.Pow(2, t.Item2)) / (2 * Math.Pow(2, t.Item2))) * (second.Center.XPosition - first.Center.XPosition);
+                double ye = first.Center.YPOsition + ((Math.Pow(2, first.Radius) - Math.Pow(2, second.Radius) + Math.Pow(2, t.Item2)) / (2 * Math.Pow(2, t.Item2))) * (second.Center.YPOsition - first.Center.YPOsition);
                 if (t.Item1 == CircleRelationship.相切)
                 {
-                    points.Add(new Point(x, y)); //两圆相切，则只有此一个交点
+                    points.Add(new Point(xe, ye)); //两圆相切，则只有此一个交点
                 }
                 else
                 {
+                    //计算出其中一个焦点到圆心之间连线与焦点之间连线的交点的距离
+                    double ce = Math.Pow(2, first.Radius) - Math.Pow(2, (xe - first.Center.XPosition)) - Math.Pow(2,(ye - first.Center.YPOsition));
+                    //两圆交点坐标
+                    double xc = xe - ce/(Math.Sqrt(1 + Math.Pow(2,k2)));
+                    double yc = ye + k2 * (xc - xe);
 
+                    double xd = xe + ce / (Math.Sqrt(1 + Math.Pow(2, k2))); ;
+                    double yd = ye + k2 * (xd - xe);
+                    //加入List
+                    points.Add(new Point(xc, yc));
+                    points.Add(new Point(xd, yd));
                 }
             }
             return points;
